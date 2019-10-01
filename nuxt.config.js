7e550c221ad6925db8e71a062db5a9685ef353d1
Kosuke Muraka <k.muraka.jp@gmail.com>
2019-10-02 03:12:54 +0900

    feat: @nuxt/dotenv の設定を追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,8 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/dotenv'
   ],
   /*
    ** Axios module configuration
