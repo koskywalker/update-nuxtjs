dca34f86a7f427fe21504b2f32b22326d5adb0a1
Kosuke Muraka <k.muraka.jp@gmail.com>
2019-10-04 23:53:13 +0900

    fix: タイトルの修正と不要な記述の削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
    */
   head: {
     titleTemplate: '%s - ' + 'UPDATE',
-    title: '世界を更新し続ける Web 制作チーム' || '',
+    title: '世界をアップデートしていく Web 制作チーム',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
