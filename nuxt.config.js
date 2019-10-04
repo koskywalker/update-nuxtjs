c59c2ecd9badf73c9d6de07542d2123f5a8bd11b
Kosuke Muraka <k.muraka.jp@gmail.com>
2019-10-04 23:56:59 +0900

    fix: タイトルの修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
    */
   head: {
     titleTemplate: '%s - ' + 'UPDATE',
-    title: '世界をアップデートしていく Web 制作チーム',
+    title: '世界をアップデートしていくクリエイターチーム',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
