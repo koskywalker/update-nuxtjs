6952b00bcd9457e023452cfd1426580b51f17bf8
koskywalker <k.muraka.jp@gmail.com>
2020-01-28 22:14:19 +0900

    refactor: グローバルメニューデータをプロパティファイルに移行

--- layouts/TheHeader.vue
+++ layouts/TheHeader.vue
@@ -41,38 +41,7 @@ export default {
   data () {
     return {
       blogName: this.$blogInfo.blogName,
-      globalNav: [
-        {
-          name: 'ホーム',
-          label: 'HOME',
-          url: '/'
-        },
-        {
-          name: 'プログラミング',
-          label: 'PROGRAMMING',
-          url: '/category/programming'
-        },
-        {
-          name: 'ブログ',
-          label: 'BLOG',
-          url: '/category/blog'
-        },
-        {
-          name: '便利ツール',
-          label: 'TOOL',
-          url: '/category/tool'
-        },
-        {
-          name: 'ポートフォリオ',
-          label: 'PORTFOLIO',
-          url: '/portfolio'
-        },
-        {
-          name: 'お問い合わせ',
-          label: 'CONTACT',
-          url: '/contact'
-        }
-      ]
+      globalNav: this.$globalMenu
     }
   },
   computed: {
