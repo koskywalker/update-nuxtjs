5f000a7cac5aee229bc9967c18dd12670f372c46
koskywalker <k.muraka.jp@gmail.com>
2020-02-02 02:48:56 +0900

    feat: nuxt-fontawesome のインストール

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ export default {
     { src: '~/plugins/blog-info.js' },
     { src: '~/plugins/my-info.js' },
     { src: '~/plugins/global-menu.js' },
+    { src: '~/plugins/footer-menu-mobile.js' },
     { src: '~/plugins/tags.js' },
     { src: '~/plugins/vue-library.js' },
     { src: '~/plugins/particles.js' },
@@ -49,7 +50,19 @@ export default {
     '@nuxtjs/pwa',
     '@nuxtjs/dotenv',
     '@nuxtjs/style-resources',
+    'nuxt-fontawesome',
   ],
+  /*
+  ** FontAwesome
+  */
+  fontawesome: {
+    imports: [
+      {
+        set: '@fortawesome/free-solid-svg-icons',
+        icons: ['fas'],
+      },
+    ],
+  },
   /*
   ** Axios module configuration
   */
