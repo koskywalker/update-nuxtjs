43d910f1fff087db0708d657a7edec41a4865e62
koskywalker <k.muraka.jp@gmail.com>
2020-02-16 11:20:27 +0900

    feat: @nuxtjs/markdownit を markdown-it に変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ export default {
     { src: '~/plugins/tags.js' },
     { src: '~/plugins/vue-library.js' },
     { src: '~/plugins/particles.js' },
+    { src: '~/plugins/markdown-it' },
   ],
   /*
   ** Nuxt.js dev-modules
@@ -50,7 +51,6 @@ export default {
     '@nuxtjs/pwa',
     '@nuxtjs/dotenv',
     '@nuxtjs/style-resources',
-    '@nuxtjs/markdownit',
     'nuxt-fontawesome',
   ],
   /*
@@ -83,20 +83,6 @@ export default {
     extend (config, ctx) {
       config.devtool = 'inline-cheap-module-source-map'
     },
-  },
-  /*
-  ** markdownit configuration
-  */
-  markdownit: {
-    preset: 'default',
-    injected: true, // $mdを利用してmarkdownをhtmlにレンダリングする
-    breaks: true, // 改行コードを<br>に変換する
-    html: true, // HTML タグを有効にする
-    linkify: true, // URLに似たテキストをリンクに自動変換する
-    typography: true, // 言語に依存しないきれいな 置換 + 引用符 を有効にします。
-    use: [
-      'markdown-it-table-of-contents',
-    ],
   },
   env: {
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
---
+++ plugins/markdown-it.js
@@ -0,0 +1,17 @@
+import MarkdownIt from 'markdown-it'
+
+export default ({ app }, inject) => {
+  const md = new MarkdownIt({
+    langPrefix: 'language-',
+    preset: 'default',
+    injected: true, // $mdを利用してmarkdownをhtmlにレンダリングする
+    breaks: true, // 改行コードを<br>に変換する
+    html: true, // HTML タグを有効にする
+    linkify: true, // URLに似たテキストをリンクに自動変換する
+    typography: true, // 言語に依存しないきれいな 置換 + 引用符 を有効にします。
+  })
+
+  md.use(require('markdown-it-table-of-contents'))
+
+  inject('md', md)
+}
