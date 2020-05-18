ffc6f90859ffb851338371d0dd10c11a8c94d8d5
koskywalker <k.muraka.jp@gmail.com>
2020-05-19 00:59:06 +0900

    feat: 動的ルーティングの設定テスト

--- nuxt.config.js
+++ nuxt.config.js
@@ -101,6 +101,7 @@ export default {
         }),
       ]).then(([ posts, tags ]) => {
         const postsNumberPerPage = 1
+        console.log(tags.items)
         return [
           ...posts.items.map((post) => {
             return { route: `posts/${post.fields.slug}`, payload: post }
@@ -111,10 +112,10 @@ export default {
           ...tags.items.map((tag) => {
             return { route: `tags/${tag.fields.slug}`, payload: tag }
           }),
-          tags.items.map((tag) => {
-            Array(Math.floor(tags.items.length / postsNumberPerPage)).fill(null).map((_, i) => {
-              return `tags/${tag.fields.slug}/${i + 2}`
-          })
+          // ...tags.items.map((tag) => {
+          //   Array(Math.floor(tags.items.length / postsNumberPerPage)).fill(null).map((_, i) => {
+          //     return `tags/${tag.fields.slug}/${i + 2}`
+          // })
         ]
       })
     },
