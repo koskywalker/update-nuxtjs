c16d3e8c5e624ccbee08204235ffb7a945b1b9ab
koskywalker <k.muraka.jp@gmail.com>
2020-06-10 00:24:27 +0900

    test

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -61,6 +61,7 @@
         />
         <div class="postFooter__comment">
           <h2>コメントしてね！</h2>
+          {{ process.env.BASE_URL }}
           <vue-disqus
             :identifier="currentPost.fields.slug"
             :url="`${baseUrl}/posts/${currentPost.fields.slug}`"
