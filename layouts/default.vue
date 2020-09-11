31e085a65ec55b0913e0aea7e4e66b41bfd4f1a3
koskywalker <k.muraka.jp@gmail.com>
2020-09-11 09:14:20 +0900

    test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <the-particles />
+    <!-- <the-particles /> -->
     <the-header />
     <main-visual v-if="$route.path === '/'" />
     <div class="container">
@@ -17,7 +17,7 @@
 </template>
 
 <script>
-import TheParticles from '@/layouts/TheParticles'
+// import TheParticles from '@/layouts/TheParticles'
 import TheHeader from '@/layouts/TheHeader'
 import TheSidebar from '@/layouts/TheSidebar'
 import TheFooter from '@/layouts/TheFooter'
@@ -25,7 +25,7 @@ import TheFooterMenuFixed from '@/layouts/TheFooterMenuFixed'
 
 export default {
   components: {
-    TheParticles,
+    // TheParticles,
     TheHeader,
     TheSidebar,
     TheFooter,
