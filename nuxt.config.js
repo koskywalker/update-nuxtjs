a32970dbb5a3f1794817e5f4b2ce2983e23256bf
koskywalker <k.muraka.jp@gmail.com>
2020-09-27 13:19:47 +0900

    [fix] next generate 時のバグを修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -146,6 +146,39 @@ export default {
       cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
     },
   },
+  generate: {
+    routes () {
+      return Promise.all([
+        client.getEntries({
+          content_type: process.env.CTF_BLOG_POST_TYPE_ID,
+        }),
+        client.getEntries({
+          content_type: 'tag',
+        }),
+      ]).then(([posts, tags]) => {
+        const postsNumberPerPage = 10
+        const tagPathList = tags.items.map((tag) => {
+          const tagPosts = posts.items.filter(post => post.fields.tags.some(postTag => postTag.sys.id === tag.sys.id))
+          const tagPostsNumber = tagPosts.length
+          return Array(Math.floor(tagPostsNumber / postsNumberPerPage)).fill(null).map((_, i) => {
+            return { route: `tags/${tag.fields.slug}/${i + 1}`, payload: tag }
+          })
+        })
+        return [
+          ...posts.items.map((post) => {
+            return { route: post.fields.slug, payload: post }
+          }),
+          ...Array(Math.floor(posts.items.length / postsNumberPerPage)).fill(null).map((_, i) => {
+            return { route: `page/${i + 1}` }
+          }),
+          ...tags.items.map((tag) => {
+            return { route: `tags/${tag.fields.slug}`, payload: tag }
+          }),
+          ...[].concat(...tagPathList),
+        ]
+      })
+    },
+  },
   env: {
     BASE_URL: process.env.BASE_URL,
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
